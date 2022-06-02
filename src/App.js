830b8f8c21f8323b48685b01e323b92fa0f84f48
Archicoder <58503130+kimyoo04@users.noreply.github.com>
2022-06-03 00:40:12 +0900

    file upload 기능 적용

--- src/App.js
+++ src/App.js
@@ -72,6 +72,25 @@ app.get("/certification", (req, res) => {
   res.render("certification");
 });
 
+app.post("/waitforauth", (req, res) => {
+  if (req.files) {
+    // console.log(req.files);
+
+    var file = req.files.file;
+    var filename = file.name;
+    console.log(filename);
+
+    // 업로드 파일 경로 이동 및 메시지 출력
+    file.mv("./uploads/" + filename, (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.render("wait_for_auth");
+      }
+    });
+  }
+});
+
 //--------------------------------------------------------------------
 
 // 서버 연결
