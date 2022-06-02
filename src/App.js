c6e026a3954ffc3749856668d9e28791d96202a9
Archicoder <58503130+kimyoo04@users.noreply.github.com>
2022-06-02 21:11:39 +0900

    react-app은 pug와 백앤드 구축 후 고민, 폴더 구조 변경 pug 작업 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from "./logo.svg";
-import "./css/App.css";
+const express = require("express");
+const upload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//--------------------------------------------------------------------
 
-export default App;
+const app = express();
+app.use(bodyParser.urlencoded({extended: false})); //
+app.use(upload());
+
+// 뷰 엔진에 퍼그 등록
+app.engine("pug", require("pug").__express);
+app.set("views", path.join(__dirname, "views")); // 현재 디렉토리에서 views폴더를 의미
+app.set("view engine", "pug");
+
+//--------------------------------------------------------------------
+
+// pug render test
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// pug file upload test
+app.post("/", (req, res) => {
+  if (req.files) {
+    // console.log(req.files);
+
+    var file = req.files.file;
+    var filename = file.name;
+    console.log(filename);
+
+    // 업로드 파일 경로 이동 및 메시지 출력
+    file.mv("./uploads/" + filename, (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("File Uploaded");
+      }
+    });
+  }
+});
+
+//--------------------------------------------------------------------
+
+// 서버 연결
+app.listen(3000, () => {
+  console.log("Connected, 3000 port!");
+});
