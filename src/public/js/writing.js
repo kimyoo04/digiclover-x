80d2347cb62683326cd0f8e3934c56c6942df743
Archicoder <58503130+kimyoo04@users.noreply.github.com>
2022-06-21 01:07:57 +0900

    MOU 문서 추가, 삭제 버튼 구현

--- src/public/js/writing.js
+++ src/public/js/writing.js
@@ -1,28 +1,90 @@
-const docuComponents = document.querySelector(".docu-contents");
-const docuComponent = document.querySelectorAll(".docu-content");
 const addContentBtn = document.querySelector(".addContentBtn");
 
-let indx = docuComponent.length;
+function mkDocuContent() {
+  // div 테그, indx 초기화
+  let docuComponents = document.querySelector(".docu-contents");
+  let docuComponent = document.querySelectorAll(".docu-content");
+  let deleteBtns = document.querySelectorAll(".deleteBtn");
+  let indx = docuComponent.length;
+  indx++;
 
-tag = `.docu-content
-    span 제 ${indx}조 (항의 이행)
-    input(type="textarea" name="additionalText")
-    `;
+  // div 생성 기능
+  createContentDIV(docuComponents, indx);
 
-addContentBtn.addEventListener("click", () => {
+  // div 삭제 기능 및 조항 indx 다시 정렬
+  deleteContentDIV(deleteBtns, indx);
+
+  // 조항 indx 다시 정렬
+  indexingInputs(indx);
+}
+
+// -------------------------------------------------------------------
+
+// div 생성 기능
+function createContentDIV(docuComponents, indx) {
+  // div 테그 생성
   let tempDiv = document.createElement("div");
   tempDiv.classList.add("docu-content");
 
-  indx++;
-  let tempSpan = document.createElement("span");
-  let node = document.createTextNode(`span 제 ${indx}조 (항의 이행)`);
-  tempSpan.appendChild(node);
+  // input 테그 생성
+  let tempInput = document.createElement("input");
+  tempInput.name = `indx`;
+  tempInput.type = `text`;
+  tempInput.value = `제 ${indx}조 (직접 작성)`;
 
+  //textarea 테그 생성
   let tempTextarea = document.createElement("textarea");
-  tempTextarea.name = indx;
+  tempTextarea.name = `content`;
 
-  tempDiv.appendChild(tempSpan);
+  // input 테그 button 생성
+  let tempDeleteBtn = document.createElement("input");
+  tempDeleteBtn.classList.add(`deleteBtn`);
+  tempDeleteBtn.type = `button`;
+  tempDeleteBtn.value = `삭제`;
+
+  // div 테그 안에 넣기
+  tempDiv.appendChild(tempInput);
   tempDiv.appendChild(tempTextarea);
+  tempDiv.appendChild(tempDeleteBtn);
 
+  // 추가 후 재할당
   docuComponents.appendChild(tempDiv);
+  docuComponent = document.querySelectorAll(".docu-content");
+  deleteBtns = document.querySelectorAll(".deleteBtn");
+}
+
+// div 삭제 기능 및 조항 indx 다시 정렬
+function deleteContentDIV(deleteBtns, indx) {
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", () => {
+      // 삭제 후 재할당
+      p = deleteBtn.parentElement;
+      p.remove();
+
+      docuComponents = document.querySelector(".docu-contents");
+      docuComponent = document.querySelectorAll(".docu-content");
+      deleteBtns = document.querySelectorAll(".deleteBtn");
+      indx--;
+
+      // 조항 indx 다시 정렬
+      indexingInputs(indx);
+    });
+  });
+}
+
+// 조항 indx 다시 정렬
+function indexingInputs(indx) {
+  let indxInputs = document.querySelectorAll("[name='indx']");
+  let contentTextareas = document.querySelectorAll("[name='content']");
+
+  for (let i = 0; i < indx; i++) {
+    if (contentTextareas[i].value === "")
+      indxInputs[i].value = `제 ${i + 1}조 (직접 작성)`;
+  }
+}
+
+// -------------------------------------------------------------------
+
+addContentBtn.addEventListener("click", () => {
+  mkDocuContent();
 });
