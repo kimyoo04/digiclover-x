512fa2fa08d05c61749811bd6e4ef9d59124ba1f
Kim Yoo <kimyoo04eco@naver.com>
2022-09-10 03:32:53 +0900

    Setting - light and dark theme & public folder load

--- frontend/src/styled.d.ts
+++ frontend/src/styled.d.ts
@@ -2,8 +2,41 @@ import "styled-components";
 
 declare module "styled-components" {
   export interface DefaultTheme {
+    // 바뀌는 색
     bgColor: string;
-    textColor: string;
+    bgWhiteColor: string;
+
+    textWhiteColor: string;
+    textblackColor: string;
+    textAcentColor: string;
+
     btnColor: string;
+
+    grayscaleblackColor: string;
+    grayscalewhiteColor: string;
+    grayscale1Color: string;
+    grayscale2Color: string;
+    grayscale3Color: string;
+    grayscale4Color: string;
+    grayscale5Color: string;
+    grayscale6Color: string;
+
+    // 바뀌지 않는 색
+    primaryGreenColor: string;
+    primaryBlueColor: string;
+
+    secondaryDarkBlueColor: string;
+    secondaryBlueColor: string;
+    secondaryMintColor: string;
+    secondaryDarkColor: string;
+    secondaryLightBlueColor: string;
+    secondaryLightMintColor: string;
+
+    infoColor: string;
+    darkVariantColor: string;
+
+    dangerColor: "#eb5757";
+    successColor: " #219653";
+    warningColor: "#f2c94c";
   }
 }
--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,13 +1,79 @@
 import {DefaultTheme} from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+export const darkTheme: DefaultTheme = {
+  // 바뀌는 색
+  bgColor: "#233e4d",
+  bgWhiteColor: "#e6ecf3",
+
+  textWhiteColor: "white",
+  textblackColor: "black",
+  textAcentColor: "teal",
+
+  btnColor: "#e0e0e0",
+
+  grayscaleblackColor: "#000",
+  grayscalewhiteColor: " #fff",
+  grayscale1Color: "#333333",
+  grayscale2Color: "#4f4f4f",
+  grayscale3Color: "#828282",
+  grayscale4Color: "#bdbdbd",
+  grayscale5Color: "#e0e0e0",
+  grayscale6Color: "#f2f2f2",
+
+  // 바뀌지 않는 색
+  primaryGreenColor: "#21ec91",
+  primaryBlueColor: "#2ea3e5",
+
+  secondaryDarkBlueColor: "#233e4d",
+  secondaryBlueColor: "#03438b",
+  secondaryMintColor: "#009cbb",
+  secondaryDarkColor: "#0e191e",
+  secondaryLightBlueColor: "#e6ecf3",
+  secondaryLightMintColor: "#e5f5f8",
+
+  infoColor: "#7d8da1",
+  darkVariantColor: "#677483",
+
+  dangerColor: "#eb5757",
+  successColor: " #219653",
+  warningColor: "#f2c94c",
 };
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "white",
-  btnColor: "teal",
+export const lightTheme: DefaultTheme = {
+  // 바뀌는 색
+  bgColor: "#e6ecf3",
+  bgWhiteColor: "#233e4d",
+
+  textWhiteColor: "black",
+  textblackColor: "white",
+  textAcentColor: "blue",
+
+  btnColor: "#4f4f4f",
+
+  grayscaleblackColor: "#fff",
+  grayscalewhiteColor: "#000",
+  grayscale1Color: "#f2f2f2",
+  grayscale2Color: "#e0e0e0",
+  grayscale3Color: "#bdbdbd",
+  grayscale4Color: "#828282",
+  grayscale5Color: "#4f4f4f",
+  grayscale6Color: "#333333",
+
+  // 바뀌지 않는 색
+  primaryGreenColor: "#21ec91",
+  primaryBlueColor: "#2ea3e5",
+
+  secondaryDarkBlueColor: "#233e4d",
+  secondaryBlueColor: "#03438b",
+  secondaryMintColor: "#009cbb",
+  secondaryDarkColor: "#0e191e",
+  secondaryLightBlueColor: "#e6ecf3",
+  secondaryLightMintColor: "#e5f5f8",
+
+  infoColor: "#f2c94c",
+  darkVariantColor: "#677483",
+
+  dangerColor: "#eb5757",
+  successColor: " #219653",
+  warningColor: "#f2c94c",
 };
